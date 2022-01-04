bfb8cdb6864a1f2707cc597c574530b103fa75fd
woody925 <immeraufempfang@protonmail.com>
2022-01-04 20:40:14 +0100

    New features of MongoDBHandler added.

--- src/database/mongodb_handler.py
+++ src/database/mongodb_handler.py
@@ -3,6 +3,8 @@ import sys
 import pymongo
 import json
 
+from pymongo import mongo_client
+
 
 class MongoDB(object):
 
@@ -13,7 +15,62 @@ class MongoDB(object):
         # having issue with this way of creating client due to authentication
         self.client = pymongo.MongoClient(host=self.host, port=self.port,
                                   maxPoolSize=self.maxPoolsize)
+        
+class MongoDBHandler:
+    mongodb_obj = None
+    operations_obj = None
+    minsert_objs = {}
 
+
+    @staticmethod
+    def init_mongodb():
+        if MongoDBHandler.is_mongodb_connection_established() != True:
+            MongoDBHandler.mongodb_obj = MongoDB(host="mongodb://rootuser:rootpass@127.0.0.1:28017")
+            print("MongoDB connection established!")
+            MongoDBHandler.init_operations()
+            MongoDBHandler.init_minsert_objs()
+        else:
+            print("MongoDB connection already established!")
+
+    @staticmethod
+    def is_mongodb_connection_established():
+        if MongoDBHandler.mongodb_obj != None: 
+            return True
+        return False
+
+    @staticmethod
+    def init_operations():
+        MongoDBHandler.operations_obj = Operations(instance=MongoDBHandler.mongodb_obj) 
+
+    @staticmethod
+    def print_all_databases():
+        if MongoDBHandler.operations_obj != None:
+            print(MongoDBHandler.operations_obj.getAllDatabase())
+        else:
+            print("Operation class instance not initialized yet!")
+    
+    @staticmethod
+    def init_minsert_objs():
+        databases = []
+        if MongoDBHandler.operations_obj != None:
+            collections = []
+            coll_dict = {}
+            databases = MongoDBHandler.operations_obj.getAllDatabase()
+            for database in databases:
+                collections = MongoDBHandler.operations_obj.mongodb.client[database].list_collection_names()
+                print("Collections of ", database, ": ", collections)
+                for collection in collections:
+                    # minsert_obj = Minsert(_instance=MongoDBHandler.mongodb_obj, dbName=database, collectionName=collection)
+                    coll_dict[collection] = Minsert(_instance=MongoDBHandler.mongodb_obj, dbName=database, collectionName=collection)
+                MongoDBHandler.minsert_objs[database] = coll_dict
+            # print(MongoDBHandler.minsert_objs["smarthome"]["sensors_log"])
+
+    @staticmethod
+    def insert_one(input_doc, db_name, coll_name):
+        if MongoDBHandler.minsert_objs[db_name][coll_name].insert_one(input_doc):
+            return True
+        else:
+            return False
 
 class Operations(object):
 
--- src/phoscon/phoscon_data_extractor.py
+++ src/phoscon/phoscon_data_extractor.py
@@ -1,5 +1,13 @@
-from phoscon_handler import Phoscon_Handler
 import json
+import os
+import sys
+
+current_dir = os.path.dirname( __file__ )
+mongodb_include_dir = os.path.join( current_dir, '..', 'database')
+sys.path.append( mongodb_include_dir )
+import mongodb_handler
+
+from phoscon_handler import Phoscon_Handler
 
 def read_temp_data(json_data, name_of_sensor):
     for x in json_data:
@@ -35,12 +43,35 @@ def fetching_data_from_sensors():
     phoscon_response = phoscon_handle.get_sensor_data_all()
     json_data = phoscon_response.json()
 
-    return_sensor_data(json_data, 'Temp-Wohnzimmer')
-    return_sensor_data(json_data, 'Temp-Badezimmer')
+    # return_sensor_data(json_data, 'Temp-Wohnzimmer')
+    # return_sensor_data(json_data, 'Temp-Badezimmer')
+    transfer_sensor_data_to_db(json_data, 'Temp-Wohnzimmer')
+    transfer_sensor_data_to_db(json_data, 'Temp-Badezimmer')
 
+def transfer_sensor_data_to_db(json_data, name_of_sensor):
+    '''transfer sensor data to db if sensor is reachable'''
+    current_sensor_values = []
+    print("########################")
+    print(json_data)
+    print("########################")
+    for x in json_data:
+        if json_data[x].get('name') == name_of_sensor and json_data[x].get('config').get('reachable') == True:
+            mongodb_handler.MongoDBHandler.insert_one(json_data[x], "smarthome", "sensors_log")
+            if 'etag' in json_data[x]:
+                current_sensor_values.append(json_data[x].get('etag'))
+            if 'temperature' in json_data[x].get('state'):
+                current_sensor_values.append((json_data[x].get('state').get('temperature')/100))
+            if 'humidity' in json_data[x].get('state'):
+                current_sensor_values.append(json_data[x].get('state').get('humidity')/100)
+            if 'pressure' in json_data[x].get('state'):
+                current_sensor_values.append(json_data[x].get('state').get('pressure'))
+    print(current_sensor_values)
+    return current_sensor_values
 
 
 print("Starting program...")
+mongodb_handler.MongoDBHandler.init_mongodb()
+
 fetching_data_from_sensors()
 print("Ending program...")
 
